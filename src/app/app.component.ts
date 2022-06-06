00bfa44d0c7d2f17f5d422c47c62a0c0ba6b1d56
akshaybogar1984 <akshayakumar.bogar@bsigroup.com>
2022-06-06 19:36:22 +0100

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Hello1 {{value}}</div>`,
 })
 export class AppComponent {
   value = 'World';
